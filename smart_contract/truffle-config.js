66155d6b6762009af29e6d4d799e78d9a60ab85f
parthgod <parthgenius.gps@gmail.com>
2024-11-15 18:36:48 +0530

    added docker

--- smart_contract/truffle-config.js
+++ smart_contract/truffle-config.js
@@ -4,7 +4,7 @@ module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     development: {
-      host: "127.0.0.1",
+      host: "0.0.0.0",
       port: 8545,
       network_id: "*",
     },
